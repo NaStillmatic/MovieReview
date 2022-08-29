7d6612177699e21fd9c24bee023b01c034a830af
Joe <nastillmatic@gmail.com>
2022-08-29 23:09:40 +0900

    first commit

---
+++ MovieReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReview
+//
+//  Created by HwangByungJo  on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
