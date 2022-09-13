5d1c6d24b63a6e1df6230113e1203ee6e72d7a98
Joe <nastillmatic@gmail.com>
2022-09-13 18:25:56 +0900

    UITest 샘플 작성

--- MovieReviewUITests/MovieReviewUITests.swift
+++ MovieReviewUITests/MovieReviewUITests.swift
@@ -9,32 +9,37 @@ import XCTest
 
 class MovieReviewUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  var app: XCUIApplication!
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+  override func setUp() {
+    super.setUp()
 
-    }
+    continueAfterFailure = false
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    app = XCUIApplication()
+    app.launch()
+  }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+  override func tearDown() {
+    super.tearDown()
+    app = nil
+  }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func test_navigationBar의_title이_영화평점으로_설정되어있다() {
+    let existNavigationBar = app.navigationBars["영화 평점"].exists
+    XCTAssertTrue(existNavigationBar)
+  }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+  func test_searchBar가_존재한다() {
+    let existSearchBar = app.navigationBars["영화 평점"].searchFields["Search"].exists
+    XCTAssertTrue(existSearchBar)
+  }
+
+  func test_searchBar에_cancel버튼이_존재한다() {
+    let navigationBar = app.navigationBars["영화 평점"]
+    navigationBar.searchFields["Search"].tap()
+
+    let existSearchBarCancelButton = navigationBar.buttons["Cancel"].exists
+    XCTAssertTrue(existSearchBarCancelButton)
+  }
 }
